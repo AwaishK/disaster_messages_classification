a208bf8801995e0c25651e9dcfc26f34b29be599
Awaish Kumar <awaish.rathi@gmail.com>
2020-06-03 14:10:31 +0200

    Update disaster.py

--- disaster_response/disaster.py
+++ disaster_response/disaster.py
@@ -1,5 +1,3 @@
 from sklearn.base import BaseEstimator, TransformerMixin
 from models.train_classifier import WordCount, tokenize
 from webapp import app
-
-app.run(host='0.0.0.0', port=3001, debug=True)
